b46f6ac758b2f83562cbf5391065f3092f40f024
Matthew Francis Ong <matthewfoys@gmail.com>
2017-11-30 12:57:14 +0800

    added QR Code reader

--- src/components/Qrcode.js
+++ src/components/Qrcode.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
+import QrReader from 'react-qr-reader'
 
 class Qrcode extends Component {
   constructor (props) {
-    super()
+    super(props)
+    this.state = {
+      delay: 300,
+      result: 'No result'
+    }
+    this.handleScan = this.handleScan.bind(this)
+  }
+  handleScan (data) {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError (err) {
+    console.error(err)
   }
   render () {
     return (
       <div>
         <h1>QR Code Scanner Goes here</h1>
+        <div style={{ width: '300px', margin: 'auto' }}>
+          <QrReader
+            delay={this.state.delay}
+            onError={this.handleError}
+            onScan={this.handleScan}
+            style={{ width: '100%' }}
+          />
+          <p>{this.state.result}</p>
+        </div>
       </div>
     )
   }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Row, Input} from 'react-materialize'
 
-
 class Register extends Component {
   constructor (props) {
     super()
@@ -11,10 +10,10 @@ class Register extends Component {
       <div>
         <h1>Registration Page</h1>
         <Row>
-		        <Input s={6} label="First Name" />
-        		<Input s={6} label="Last Name" /> 
-        		<Input type="password" label="password" s={12} />
-        		<Input type="email" label="Email" s={12} />
+          <Input s={6} label='First Name' />
+          <Input s={6} label='Last Name' />
+          <Input type='password' label='password' s={12} />
+          <Input type='email' label='Email' s={12} />
         </Row>
       </div>
     )
