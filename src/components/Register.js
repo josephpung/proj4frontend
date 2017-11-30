dc271dc0a37a50cb5081e22f02fbec5f94465695
josephpung <goldenpummel@live.com>
2017-11-30 10:40:01 +0800

    added default views

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react'
-import { Row, Input} from 'react-materialize'
+import { Row, Input, Button} from 'react-materialize'
 
 
 class Register extends Component {
   constructor (props) {
     super()
+
+    this.state = {
+      submitObj: {}
+      }
+  }
+  handleChange(e){
+    let tempObj = {...this.state.submitObj}
+    tempObj[e.target.name] = e.target.value
+     this.setState({
+       submitObj: tempObj
+     })
   }
   render () {
     return (
       <div>
         <h1>Registration Page</h1>
         <Row>
-		        <Input s={6} label="First Name" />
-        		<Input s={6} label="Last Name" /> 
-        		<Input type="password" label="password" s={12} />
-        		<Input type="email" label="Email" s={12} />
+		        <Input s={6} name='name'label="Name" onChange={(e) => this.handleChange(e)}/>
+            <Input type="email" name='email' label="Email" s={12} onChange={(e) => this.handleChange(e)}/>
+        		<Input type="password" name='password' label="Password (Min 8 char)" s={12} onChange={(e) => this.handleChange(e)}/>
+        		<Input type="password" name='cfmPassword' label="Confirm Password" s={12} onChange={(e) => this.handleChange(e)} />
+        </Row>
+        <Row>
+            <Button waves='light'>Submit</Button>
         </Row>
       </div>
     )
